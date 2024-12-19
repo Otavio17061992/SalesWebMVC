c41c252f9f7982764f6425c9f8a4875100d14077
Jon.Ocx <joao.almeida48@outlook.com>
2024-12-19 11:00:00 -0300

    Validation in Data Seller

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,16 +9,23 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Range(1000,50000,ErrorMessage ="{0} must be from {1} to {2}")]
+        [Required(ErrorMessage ="{0} Required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
+        [EmailAddress(ErrorMessage ="Enter a valid Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
